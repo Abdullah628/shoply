e84ab656735824a4144e30264565eda32300282a
abdullah628 <abdullah628360@gmail.com>
2024-02-27 20:12:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Banner from './components/Banner/Banner'
+
+const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(true);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar handleOrderPopup = {handleOrderPopup}/>
+      <Hero handleOrderPopup = {handleOrderPopup}/>
+      <Banner/>
+    </div>
+  );
+};
+
+export default App;
